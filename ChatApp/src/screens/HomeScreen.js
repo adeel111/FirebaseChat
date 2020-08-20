c13d6f28a6424d01f26fd532e9d0f46532661287
muhamad_ashfaq <ashfaq441997@gmail.com>
2020-08-20 18:59:49 +0500

    practing chat feature with firebase

---
+++ ChatApp/src/screens/HomeScreen.js
@@ -0,0 +1,73 @@
+import React, {useState, useEffect, useContext} from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
+import {List, Divider} from 'react-native-paper';
+import firestore from '@react-native-firebase/firestore';
+import Loading from '../components/Loading';
+
+export default function HomeScreen() {
+  const [threads, setThreads] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = firestore()
+      .collection('THREADS')
+      .onSnapshot((querySnapshot) => {
+        const threads = querySnapshot.docs.map((documentSnapshot) => {
+          return {
+            _id: documentSnapshot.id,
+            // give defaults
+            name: '',
+            ...documentSnapshot.data(),
+          };
+        });
+
+        setThreads(threads);
+
+        if (loading) {
+          setLoading(false);
+        }
+      });
+
+    /**
+     * unsubscribe listener
+     */
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={threads}
+        keyExtractor={(item) => item._id}
+        ItemSeparatorComponent={() => <Divider />}
+        renderItem={({item}) => (
+          <List.Item
+            title={item.name}
+            description="Item description"
+            titleNumberOfLines={1}
+            titleStyle={styles.listTitle}
+            descriptionStyle={styles.listDescription}
+            descriptionNumberOfLines={1}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+  },
+  listTitle: {
+    fontSize: 22,
+  },
+  listDescription: {
+    fontSize: 16,
+  },
+});
