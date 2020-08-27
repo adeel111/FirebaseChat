a1cad3116df92a00fea6ae41773cb9bdfded2e6f
muhamad_ashfaq <ashfaq441997@gmail.com>
2020-08-27 17:29:13 +0500

    installed required libs

--- AudioPlayer/App.js
+++ AudioPlayer/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, Text, StatusBar} from 'react-native';
+import TrackPlayer from 'react-native-track-player';
 
 const App = () => {
+  useEffect(() => {
+    (async () => {
+      await TrackPlayer.setupPlayer().then(() => {
+        // alert('Setup Successfully');
+        console.log('Setup Successfully');
+      });
+    })();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
