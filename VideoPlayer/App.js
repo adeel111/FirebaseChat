3511ac0cfa476312a94c45a74aa2a1e7afb7a7eb
muhamad_ashfaq <ashfaq441997@gmail.com>
2020-08-27 14:29:00 +0500

    created project

---
+++ VideoPlayer/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Welcome to Video Player world!</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
