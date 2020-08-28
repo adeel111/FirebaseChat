c730fc6ef8bf4f0793959a7e32e14dad93d4a362
muhamad_ashfaq <ashfaq441997@gmail.com>
2020-08-28 12:45:06 +0500

    created project

---
+++ NavFive/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Welcome to NavFive</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
