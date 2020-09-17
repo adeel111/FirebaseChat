0a3f1dcf72eb177c1c1d018ac5764e5dd3a7a682
adeel11 <adeeliftikhar67@gmail.com>
2020-09-17 18:16:07 +0500

    testing for commit owner

--- PayPal/App.js
+++ PayPal/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ByWeb from './screens/ByWeb'; // done
 import ByLib from './screens/ByLib'; // pending (no need at the moment)
 import CheckoutCards from './screens/CheckoutCards'; // done
-import Multibanco from './screens/Multibanco'; // doing
+import Multibanco from './screens/Multibanco'; // doing...
 
 class App extends Component {
   render() {
